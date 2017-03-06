c442d7ce6221e5c2a8987402d321c33e95a48e69
jsreyes <ingjsreyesm@gmail.com>
2017-03-05 22:00:28 -0500

    Archivos generados por la última versión del Generator-OAS.

---
+++ oikos_cliente/app/scripts/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name oikosClienteApp
+ * @description
+ * # oikosClienteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('oikosClienteApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'afOAuth2',
+    'treeControl',
+    'ngMaterial',
+    'ui.grid',
+    'ui.grid.edit',
+    'ui.grid.rowEdit',
+    'ui.grid.cellNav',
+    'ui.grid.treeView',
+    'ui.grid.selection',
+    'ui.grid.exporter',
+    'ngStorage',
+    'ngWebSocket',
+    'angularMoment',
+    'ui.utils.masks'
+  ])
+    .run(function(amMoment) {
+      amMoment.changeLocale('es');
+    })
+    .config(['$locationProvider','$routeProvider', function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix("");
+      $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/notificaciones', {
+        templateUrl: 'views/notificaciones.html',
+        controller: 'NotificacionesCtrl',
+        controllerAs: 'notificaciones'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
