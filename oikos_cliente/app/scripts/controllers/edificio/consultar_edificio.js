0f4be582e40dbf11d5088ed80a8d82f8a1dcf863
jsreyes <ingjsreyesm@gmail.com>
2017-03-05 22:48:45 -0500

    Se crea correspondiente a los controladores de edificio en la carpeta scripts

---
+++ oikos_cliente/app/scripts/controllers/edificio/consultar_edificio.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oikosClienteApp.controller:ConsultarEdificioCtrl
+ * @description
+ * # ConsultarEdificioCtrl
+ * Controller of the oikosClienteApp
+ */
+angular.module('oikosClienteApp')
+  .controller('ConsultarEdificioCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
